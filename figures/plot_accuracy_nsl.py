5a48456e0c54289c3b0847aafbb6334c779d16c4
littlepretty@XcodeMac <littlepretty881203@gmail.com>
2018-02-10 22:42:18 -0600

    add figures and evaluation
    Signed-off-by: littlepretty@XcodeMac <littlepretty881203@gmail.com>

--- figures/plot_accuracy_nsl.py
+++ figures/plot_accuracy_nsl.py
@@ -14,31 +14,34 @@ def autolabel(rects):
                 '%.1f' % height, ha='center', va='bottom')
 
 matplotlib.rc('font', size=20)
-machines =       ['SVM',   'MLP',   'RBM',   'SAE',   'WnD']
-train_accuracy = [99.2077, 99.3966, 99.4912, 99.3449, 99.7261]
-test_accuracy =  [78.5353, 78.3521, 78.8347, 79.1519, 78.9567]
-train_std =      [0.00000, 0.0246,  0.0231,  0.0216,  0.0121]
-test_std =       [0.00000, 0.5162,  0.3499,  0.4215,  0.0813]
+machines =  ['SVM',   'MLP',   'RBM',   'SAE',   'WnD']
+accuracy  = [78.5353, 78.3521, 78.8058, 79.1519, 78.9567]
+precision = [81.5500, 82.0900, 82.0100, 82.1300, 80.0000]
+recall    = [78.5400, 78.6600, 78.8100, 79.3400, 78.0000]
+# train_accuracy = [99.2077, 99.3966, 99.4912, 99.3449, 99.7261]
+# train_std =      [0.00000, 0.0246,  0.0231,  0.0216,  0.0121]
+# test_std =       [0.00000, 0.5162,  0.3499,  0.4215,  0.0813]
 
-width = 0.4
-ind = np.arange(0, len(machines))
+width = 0.93
+ind = np.arange(0, len(machines) * 3, 3)
 fig, ax = plt.subplots()
-rects1 = ax.bar(ind, train_accuracy, width, yerr=train_std,
-                color='b', error_kw=dict(ecolor='g'),
-                hatch='/', label='Train')
-rects2 = ax.bar(ind + width, test_accuracy, width, yerr=test_std,
-                color='r', error_kw=dict(ecolor='g'),
-                hatch='\\', label='Test')
-ax.set_ylabel('Accuracy(%)')
+rects1 = ax.bar(ind, accuracy, width, color='r', hatch='/', label='Accuracy')
+rects2 = ax.bar(ind + width, precision, width, color='g',
+                hatch='\\', label='Precision')
+rects3 = ax.bar(ind + 2 * width, recall, width, color='b',
+                hatch='-', label='Recall')
+
+ax.set_ylabel('Testset Performance(%)')
 ax.set_xlabel('Classifiers')
 ax.set_xticks(ind + width)
 ax.set_xticklabels(machines)
-ax.set_xlim([-0.5, len(machines)])
-ax.set_ylim([65, 102])
+ax.set_xlim([-1.5, len(machines) * 3])
+ax.set_ylim([60, 85])
 plt.grid(linestyle=':')
 plt.legend(loc='lower right')
 autolabel(rects1)
 autolabel(rects2)
+autolabel(rects3)
 plt.tight_layout()
-plt.savefig('comp_accuracy_nsl.pdf', fmt='pdf')
+# plt.savefig('comp_accuracy_nsl.pdf', fmt='pdf')
 plt.show()
--- figures/plot_accuracy_unsw.py
+++ figures/plot_accuracy_unsw.py
@@ -15,30 +15,33 @@ def autolabel(rects):
 
 matplotlib.rc('font', size=20)
 machines =   ['SVM',   'MLP',   'RBM',   'SAE',   'WnD']
-train_accu = [93.6306, 94.3630, 94.3947, 94.3969, 93.5365]
-test_accu =  [81.5041, 86.9926, 87.0168, 87.7393, 90.3245]
-train_std =  [0.00000, 0.23000, 0.00033, 0.00025, 2.28320]
-test_std =   [0.00000, 1.01820, 0.00731, 0.01192, 3.17770]
+accuracy =   [81.5041, 86.8216, 87.1252, 88.0666, 91.1513]
+precision =  [81.5000, 88.8800, 88.6200, 89.2700, 92.4562]
+recall    =  [80.5000, 86.8200, 87.1300, 88.0700, 90.9821]
+# train_accu = [93.6306, 94.3630, 94.3947, 94.3969, 93.5365]
+# train_std =  [0.00000, 0.23000, 0.00033, 0.00025, 2.28320]
+# test_std =   [0.00000, 1.01820, 0.00731, 0.01192, 3.17770]
 
-width = 0.4
-ind = np.arange(len(machines))
+width = 0.93
+ind = np.arange(0, len(machines) * 3, 3)
 fig, ax = plt.subplots()
-rects1 = ax.bar(ind, train_accu, width, yerr=train_std,
-                color='b', error_kw=dict(ecolor='g'),
-                hatch='/', label='Train')
-rects2 = ax.bar(ind + width, test_accu, width, yerr=test_std,
-                color='r', error_kw=dict(ecolor='g'),
-                hatch='\\', label='Test')
-ax.set_ylabel('Accuracy(%)')
+rects1 = ax.bar(ind, accuracy, width, color='r', hatch='/', label='Accuracy')
+rects2 = ax.bar(ind + width, precision, width, color='b', alpha=0.4,
+                hatch='\\', label='Precision')
+rects3 = ax.bar(ind + 2 * width, recall, width, color='g',
+                hatch='-', label='Recall')
+
+ax.set_ylabel('Testset Performance(%)')
 ax.set_xlabel('Classifiers')
 ax.set_xticks(ind + width)
 ax.set_xticklabels(machines)
-ax.set_xlim([-0.5, len(machines)])
-ax.set_ylim([65, 100])
+ax.set_xlim([-1.5, len(machines) * 3])
+ax.set_ylim([60, 95])
 plt.grid(linestyle=':')
 plt.legend(loc='lower right')
 autolabel(rects1)
 autolabel(rects2)
+autolabel(rects3)
 plt.tight_layout()
-plt.savefig('comp_accuracy_unsw.pdf', fmt='pdf')
+# plt.savefig('comp_accuracy_unsw.pdf', fmt='pdf')
 plt.show()
